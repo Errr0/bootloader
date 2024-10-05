e97f52adc418e53c75fd92a4eb315e28157f12cc
Errr0 <fatalerror404.6@gmail.com>
2024-10-05 21:10:39 +0200

    printing

--- src/boot.asm
+++ src/boot.asm
@@ -8,7 +8,34 @@ main:
     mov ss,ax
 
     mov sp,0x7c00
-
+    mov si,msg
+    call print
 jmp $
+;end of program functions below
+print:
+    push si
+    push ax
+    push bx
+
+print_loop:
+    lodsb
+    or al,al
+    jz done_print
+
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+    
+    jmp print_loop 
+
+done_print:
+    pop bx 
+    pop ax
+    pop si
+    ret
+
+msg: db 'OS has booted', 0x0d, 0x0a, 0
+
+
 times 510 - ($ - $$) db 0
 dw 0xaa55
